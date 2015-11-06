01399eef3f546dcee3d40c31847df4f0b03983b3
Raimondas Rimkus <narimantukas@gmail.com>
2015-11-06 21:08:52 +0200

    Added basic file open

--- app/src/main/java/rkr/drive/notepad/DocumentList.java
+++ app/src/main/java/rkr/drive/notepad/DocumentList.java
@@ -1,31 +1,114 @@
 package rkr.drive.notepad;
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class DocumentList extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.DriveFile;
+import com.google.android.gms.drive.DriveId;
+import com.google.android.gms.drive.OpenFileActivityBuilder;
+
+
+public class DocumentList extends BaseDriveActivity {
+
+    private DriveId mCurrentDriveId;
+    protected static final int REQUEST_CODE_OPENER = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_document_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                IntentSender intent = Drive.DriveApi
+                        .newOpenFileActivityBuilder()
+                        .build(mGoogleApiClient);
+                try {
+                    startIntentSenderForResult(intent, REQUEST_CODE_OPENER, null, 0, 0, 0);
+                } catch (IntentSender.SendIntentException e) {
+                    Log.w("DocumentList", "Unable to send intent", e);
+                }
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CODE_OPENER:
+                if (resultCode == RESULT_OK) {
+                    Log.d("DocumentList", "In Open activity result");
+                    mCurrentDriveId = data.getParcelableExtra(OpenFileActivityBuilder.EXTRA_RESPONSE_DRIVE_ID);
+                    get();
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void get() {
+        Log.d("DocumentList", "Retrieving...");
+        DriveFile file = mCurrentDriveId.asDriveFile();
+
+        //final PendingResult<DriveResource.MetadataResult> metadataResult = file.getMetadata(mGoogleApiClient);
+        final PendingResult<DriveApi.DriveContentsResult> contentsResult = file.open(mGoogleApiClient, DriveFile.MODE_READ_ONLY, null);
+
+        contentsResult.setResultCallback(onContentsCallback);
+    }
+
+    ResultCallback<DriveApi.DriveContentsResult> onContentsCallback =
+            new ResultCallback<DriveApi.DriveContentsResult>() {
+                @Override
+                public void onResult(DriveApi.DriveContentsResult result) {
+                    Log.d("DocumentList", "In file open");
+                    String contents = Utils.readFromInputStream(result.getDriveContents().getInputStream());
+                    TextView backGroundText = (TextView) findViewById(R.id.text_background);
+                    backGroundText.setText(contents);
+                }
+            };
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        super.onConnected(connectionHint);
+        Toast.makeText(this, "Connected", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        super.onConnectionFailed(result);
+        Toast.makeText(this, "Failed to connect", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 
     @Override
---
+++ app/src/main/java/rkr/drive/notepad/TextEditor.java
@@ -0,0 +1,13 @@
+package rkr.drive.notepad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextEditor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_editor);
+    }
+}
